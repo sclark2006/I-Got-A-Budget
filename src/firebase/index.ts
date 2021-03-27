fc75477638b7b4eca3b78e70ebe35a71af6ed440
Frederick Clark <sclark2006@gmail.com>
2021-03-27 10:10:09 -0400

    fix: Firebase connection fixed

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -1,15 +1,7 @@
 import firebase from 'firebase';
-const config = {
-    apiKey: "AIzaSyBVpmbDr1tyCcluCmOHxYGS01Bx8QESS8Q",
-    authDomain: "i-got-a-budget.firebaseapp.com",
-    projectId: "i-got-a-budget",
-    storageBucket: "i-got-a-budget.appspot.com",
-    messagingSenderId: "757992028428",
-    appId: "1:757992028428:web:6b46be3d79bcdf413ef129",
-    measurementId: "G-T19S5BNXHQ"
-}
+import config from '../firebase.config';
 
-const db = firebase.initializeApp(config).firestore();
+const db = firebase.initializeApp(config.firebase).firestore();
 firebase.analytics();
 
 export default db;
